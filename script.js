f5931fba5c03300715e5758038b8d6b3e35f86dc
Tea <117409782+someonecantcode@users.noreply.github.com>
2023-09-17 20:11:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const trailer = document.getElementbyId("trailer");
+const trailer = document.getElementById("trailer");
 
 const animateTrailer = (e) => {
   const x = e.clientX - trailer.offsetWidth / 2,
         y = e.clientY - trailer.offsetHeight / 2;
 
   const keyframes = {
-    transform: `translate(${x}px, ${y}px)`;
+    transform: `translate(${x}px, ${y}px)`
   }
 
   trailer.animate(keyframes, {
     duration: 800,
     fill: "forwards"
-  }
+  });
   
 }
 
 window.onmousemove = e => {
-
   animateTrailer(e);
 }
